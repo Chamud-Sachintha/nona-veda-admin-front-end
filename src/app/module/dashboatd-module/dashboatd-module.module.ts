21149e99aa06394631e920f40ead77d18a85a22f
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-09-13 11:42:24 +0530

    initla comit with modules

---
+++ src/app/module/dashboatd-module/dashboatd-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboatdModuleRoutingModule } from './dashboatd-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DashboatdModuleRoutingModule
+  ]
+})
+export class DashboatdModuleModule { }
